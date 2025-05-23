b9af56155c52034525daa557bb6b200932aee5cb
Ledmington <ledmington.dev@gmail.com>
2025-05-23 19:42:20 +0200

    Created skeleton of cli application

--- README.md
+++ README.md
@@ -8,17 +8,17 @@ First make sure the parser generator is built:
 
 Then, make it generate the parser from your EBNF grammar file:
 ```bash
-java -jar cli/build/libs/parser-gen-cli-0.1.0.jar <your_grammar_file> -o your/package/MyParser.java
+java -jar cli/build/libs/parser-gen-cli-0.1.0.jar -g <your_grammar_file> -o your/package/MyParser
 ```
 
 Finally, use it in your code like so:
 ```bash
 package your.package;
 public class MyClass {
-  public static void main(String[] args) {
-    final String text = Files.readString(args[0]);
-    System.out.println(MyParser.parse(text));
-  }
+    public static void main(String[] args) {
+        final String text = Files.readString(args[0]);
+        System.out.println(MyParser.parse(text));
+    }
 }
 ```
 
--- cli/src/main/java/com/ledmington/cli/Main.java
+++ cli/src/main/java/com/ledmington/cli/Main.java
@@ -17,8 +17,82 @@
  */
 package com.ledmington.cli;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+
+import com.ledmington.ebnf.Node;
+import com.ledmington.ebnf.Parser;
+
 public class Main {
+
+	private static void die(final String format, final Object... args) {
+		System.err.printf(format, args);
+		System.exit(-1);
+	}
+
 	public static void main(final String[] args) {
-		System.out.println("Hello world");
+		String grammarFile = null;
+		String outputFile = null;
+
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-g")) {
+				i++;
+				if (grammarFile != null) {
+					die("Cannot set grammar file twice, was already '%s'.%n", grammarFile);
+				}
+				grammarFile = args[i];
+			} else if (args[i].equals("-o")) {
+				i++;
+				if (outputFile != null) {
+					die("Cannot set output file twice, was already '%s'.%n", outputFile);
+				}
+				outputFile = args[i];
+			} else {
+				die("Unknown command-line argument: '%s'.%n", args[i]);
+			}
+		}
+
+		if (grammarFile == null) {
+			die("No grammar file was set.%n");
+		}
+		if (outputFile == null) {
+			die("No output file was set.%n");
+		}
+
+		try {
+			final Node root = Parser.parse(Files.readString(Path.of(Objects.requireNonNull(grammarFile))));
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		final Path outputPath = Path.of(Objects.requireNonNull(outputFile) + ".java")
+				.normalize()
+				.toAbsolutePath();
+		try (final BufferedWriter bw =
+				Files.newBufferedWriter(outputPath, StandardCharsets.UTF_8, StandardOpenOption.CREATE_NEW)) {
+			final int idx = outputFile.lastIndexOf(File.separator);
+			final String className = idx < 0 ? outputFile : outputFile.substring(idx + 1);
+			final String packageName =
+					idx < 0 ? "UnknownPackage" : outputFile.substring(0, idx).replace(File.separator, ".");
+			bw.write(String.join(
+					"\n",
+					"package " + packageName + ";",
+					"public final class " + className + " {",
+					"public record Node() {}",
+					"private " + className + "(){}",
+					"public static Node parse(final String input) {",
+					"if(input.equals(\"a\")){return new Node();}",
+					"else {return null;}",
+					"}",
+					"}"));
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
