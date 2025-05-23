ab3bad0f1c15ec5022f11692b8a734ad4c1b8ef6
Ledmington <ledmington.dev@gmail.com>
2025-05-23 19:08:56 +0200

    Updated license header

--- ebnf/src/main/java/com/ledmington/ebnf/Symbols.java
+++ ebnf/src/main/java/com/ledmington/ebnf/Symbols.java
@@ -1,6 +1,6 @@
 /*
  * parser-gen - Parser Generator
- * Copyright (C) 2023-2025 Filippo Barbari <filippo.barbari@gmail.com>
+ * Copyright (C) 2025-2025 Filippo Barbari <filippo.barbari@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
