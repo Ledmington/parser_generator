2b6a119cfdcad9feb90279589592d7729be695f5
Ledmington <ledmington.dev@gmail.com>
2025-07-10 23:38:33 +0200

    Added new integration testing infrastructure

---
+++ integration/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ $# -ne 1 ]] ; then
+  echo "Expected the jar file to run tests against."
+  exit 1
+fi
+
+JAR_FILE=$1
+INTEGRATION_TEST_DIR="$(dirname "${BASH_SOURCE[0]}")"
+for test_dir in "${INTEGRATION_TEST_DIR}"/*/ ; do
+  cd "${test_dir}"
+  java -jar "${JAR_FILE}" -g grammar -o /tmp/Main.java -m
+  java /tmp/Main.java input > /tmp/actual
+  diff /tmp/actual expected
+  echo "${test_dir}... OK"
+  cd ..
+done
